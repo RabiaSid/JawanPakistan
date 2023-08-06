6018586c72ae7877a72916e6ca704e2c0b68e55e
RabiaSid <rabiasid984@gmail.com>
2023-08-07 04:40:18 +0500

    changes on assignment 2

--- Assignment2/script.js
+++ Assignment2/script.js
@@ -1,31 +1,129 @@
-
-const mobilePhones = [
-    { Model: "iPhone 13", Company: "Apple", Price: "$999", Camera: "12 MP" },
-    { Model: "Galaxy S21", Company: "Samsung", Price: "$899", Camera: "108 MP" },
-    { Model: "Pixel 6", Company: "Google", Price: "$799", Camera: "50 MP" },
-    
+let arr = [
+  {
+    brand: "Samsung",
+    model: "A30",
+    price: "40000",
+    camera: "40px",
+    ram: "8gb",
+    rom: "128gb",
+  },
+  {
+    brand: "Samsung",
+    model: "A10",
+    price: "40000",
+    camera: "40px",
+    ram: "4gb",
+    rom: "128gb",
+  },
+  {
+    brand: "Samsung",
+    model: "A20",
+    price: "40000",
+    camera: "40px",
+    ram: "4gb",
+    rom: "128gb",
+  },
+  {
+    brand: "Vivo",
+    model: "Y20",
+    price: "20000",
+    camera: "40px",
+    ram: "2gb",
+    rom: "32gb",
+  },
+  {
+    brand: "Vivo",
+    model: "Y11",
+    price: "20000",
+    camera: "40px",
+    ram: "3gb",
+    rom: "32gb",
+  },
+  {
+    brand: "Vivo",
+    model: "Y15",
+    price: "30000",
+    camera: "40px",
+    ram: "4gb",
+    rom: "64gb",
+  },
+  {
+    brand: "Motorola",
+    model: "123",
+    price: "5000",
+    camera: "2px",
+    ram: "2gb",
+    rom: "32gb",
+  },
+  {
+    brand: "Iphone",
+    model: "12",
+    price: "300000",
+    camera: "40px",
+    ram: "8gb",
+    rom: "128gb",
+  },
+  {
+    brand: "Iphone",
+    model: "13",
+    price: "300000",
+    camera: "40px",
+    ram: "8gb",
+    rom: "128gb",
+  },
+  {
+    brand: "Iphone",
+    model: "X",
+    price: "300000",
+    camera: "40px",
+    ram: "8gb",
+    rom: "128gb",
+  },
 ];
 
-function filterMobiles() {
-    const searchBy = document.getElementById("searchBy").value;
-    const searchValue = document.getElementById("searchValue").value.toLowerCase();
+let brandSelect = document.getElementById("company");
+let modelselect = document.getElementById("model");
 
-    const filteredMobiles = mobilePhones.filter(phone => phone[searchBy].toLowerCase().includes(searchValue));
+let brands = arr.map((x) => x.brand);
+// remove duplication
+brands = [...new Set(brands)];
+console.log(brands);
 
-    displayResults(filteredMobiles);
-}
+brandSelect.innerHTML += brands.map(
+  (x) => `<option value="${x}">${x}</option>`
+);
 
-function displayResults(mobiles) {
-    const resultsDiv = document.getElementById("results");
-    resultsDiv.innerHTML = "";
+let modals = arr.map((x) => x.model);
+// remove duplication
+modals = [...new Set(modals)];
+console.log(modals);
 
-    if (mobiles.length === 0) {
-        resultsDiv.innerText = "No matching results.";
-    } else {
-        mobiles.forEach(phone => {
-            const phoneDetails = document.createElement("p");
-            phoneDetails.innerText = `Model: ${phone.Model}, Company: ${phone.Company}, Price: ${phone.Price}, Camera: ${phone.Camera}`;
-            resultsDiv.appendChild(phoneDetails);
-        });
-    }
-}
+modelselect.innerHTML += modals.map(
+  (x) => `<option value="${x}">${x}</option>`
+);
+
+
+let selectBrand = () => {
+  let selectedBrand = brandSelect.value;
+  let models = arr.filter((x) => x.brand == selectedBrand);
+  console.log(models);
+  modelselect.innerHTML = "";
+    modelselect.innerHtml += models.map(
+      (x) => `<option value="${x.model}">${x.model}</option>`
+    );
+};
+
+let tableBody = document.getElementById("tableBody");
+tableBody.innerHTML += arr.map(
+  (x) => `
+<tr>
+<td>${x.brand}</td>
+<td>${x.model}</td>
+<td>${x.camera}</td>
+<td>${x.price}</td>
+<td>${x.ram}</td>
+<td>${x.rom}</td>
+</tr>`
+);
+
+
